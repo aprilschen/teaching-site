ab8d58fa1bace67dd75a20c185a4af6fb97bc2d0
Shiwei Chen <april.chen.0615@gmail.com>
2023-04-22 20:31:39 -0700

    Folder Structure Setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Teaching Portal
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
