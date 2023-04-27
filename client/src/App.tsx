b252fd6a5eeee6a8ce7c3fc4f39436df263567e1
Shiwei Chen <april.chen.0615@gmail.com>
2023-04-27 16:02:29 -0700

    Replit and Stripe Integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,18 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Shiwei's tutoring portal</h1>
       <p>Currently under construction! Come back later! {`<3`}</p>
       <div className="card">
-        <p>Click below!</p>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button>
+          <a href="https://buy.stripe.com/aEUg2pe2t16CgOk6oo" target="_blank">Pay Tuition</a>
         </button>
       </div>
+
+      <iframe width="1200vw" height="800vh" src="https://replit.com/@schen-academy/VainDazzlingField"></iframe>
+
     </>
   )
 }
