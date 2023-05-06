8813b58fc97fdd0b640fc66f2e0f6ad06b17fee2
Shiwei Chen <april.chen.0615@gmail.com>
2023-05-05 22:27:08 -0700

    Better footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,11 +1,26 @@
-import { BottomNavigation } from '@mui/material';
-
+import { Box, Typography } from '@mui/material';
+import CopyrightIcon from '@mui/icons-material/Copyright';
 export default function Footer(props: any) {
     return (
-        <BottomNavigation sx={(props.theme.palette.mode == 'dark' ?
-        {backgroundColor: '#111111', minHeight: '15vh'}:
-        {backgroundColor: '#DDDDDD', minHeight: '15vh'})}
+        <Box sx={(props.theme.palette.mode == 'dark' ?
+        {backgroundColor: '#111111', py:5}:
+        {backgroundColor: '#DDDDDD', py:5})}
+        component="footer">
+        <Typography variant="h6" align="center" gutterBottom
+        sx={(props.theme.palette.mode == 'dark' ?
+        {color: '#FFFFFF'}:
+        {color: '#111111'})}>
+            Placeholder Footer
+        </Typography>
+
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
         >
-        </BottomNavigation>
+          Made with 💜 by April Chen, 2023
+        </Typography>
+      </Box>
     );
 }
