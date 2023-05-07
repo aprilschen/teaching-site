2555859bd4d55961887604c6495df01a5ff80b1b
Shiwei Chen <april.chen.0615@gmail.com>
2023-05-06 17:44:08 -0700

    Improve Footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,16 +1,14 @@
-import { Box, Typography } from '@mui/material';
+import { Box, IconButton, Link, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
 export default function Footer(props: any) {
     return (
         <Box sx={(props.theme.palette.mode == 'dark' ?
         {backgroundColor: '#111111', py:5}:
         {backgroundColor: '#DDDDDD', py:5})}
         component="footer">
-        <Typography variant="h6" align="center" gutterBottom
-        sx={(props.theme.palette.mode == 'dark' ?
-        {color: '#FFFFFF'}:
-        {color: '#111111'})}>
-            Placeholder Footer
-        </Typography>
 
         <Typography
           variant="subtitle1"
@@ -20,6 +18,21 @@ export default function Footer(props: any) {
         >
           Made with 💜 by April Chen, 2023
         </Typography>
+
+        <Box
+        sx={(props.theme.palette.mode == 'dark' ?
+        {textAlign: 'center', color: '#FFFFFF'}:
+        {textAlign: 'center', color: '#111111'})}>
+            <IconButton href='https://github.com/aprilschen' target='_blank'>
+                <GitHubIcon/>
+            </IconButton>
+            <IconButton href='https://www.linkedin.com/in/schen15/' target='_blank'>
+                <LinkedInIcon/>
+            </IconButton>
+            <IconButton href='https://www.instagram.com/shiwei_chen06' target='_blank'>
+                <InstagramIcon/>
+            </IconButton>
+        </Box>
       </Box>
     );
 }
