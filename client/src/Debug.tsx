02850e35cc96318b242a4685038bbc90f016850b
Shiwei Chen <april.chen.0615@gmail.com>
2023-05-14 11:43:09 -0700

    Integrated Backend API

--- client/src/Debug.tsx
+++ client/src/Debug.tsx
@@ -6,16 +6,16 @@ import { useEffect, useState } from "react";
 export default function Debug() {
     const [user, setUser] = useState("");
     useEffect(() => {
-        axios.get("https://tlixtrcr2c.execute-api.us-west-1.amazonaws.com/server/user/2")
+        axios.get("https://0l3iu0rscb.execute-api.us-west-1.amazonaws.com/dev/student/1")
         .then(res => stringify(res.data))
         .then(data => setUser(data))
     }, [])
 
     return (
         <>
-        <Paper>
+        <Paper sx={{mx:3, p:2}}>
             <Typography>Debugging Page</Typography>
-            <Typography>The number below is passed in from the express server</Typography>
+            <Typography>The number below is passed in from the express server:</Typography>
             <Typography>{user}</Typography>
         </Paper>
         </>
